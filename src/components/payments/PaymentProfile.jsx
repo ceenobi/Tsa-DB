218ec43b5eaeda24a93db2ea784b564a2c14fc3e
ceenobi <cobimbachu@gmail.com>
2024-03-31 23:33:30 +0100

    more design fix

--- src/components/payments/PaymentProfile.jsx
+++ src/components/payments/PaymentProfile.jsx
@@ -75,129 +75,132 @@ export default function PaymentProfile({
             {error.message ? error.message : error}
           </span>
         )}
-        {isLoading && <Spinner />}
+
         {!isLoading && !isError && !student && (
           <span className="text-red-400">
             You have no student data to display
           </span>
         )}
-
-        <div>
-          <>
-            <Row
-              className={`rounded-3 m-2 p-3 align-items-center ${styles.paymentBg}`}
-            >
-              <Col xs={6} lg={3}>
-                <div
-                  style={{ color: "var(--deepBlack)" }}
-                  className="fw-bold mb-1"
-                >
-                  {student.fullName}
-                </div>
-                <p style={{ color: "var(--offBlack)" }}>
-                  {student.courseCohort}
-                </p>
-              </Col>
-              <Col xs={6} lg={3}>
-                <p style={{ color: "var(--offBlack)" }} className="mb-0">
-                  Course Fee
-                </p>
-                <p style={{ color: "var(--mainBlue)" }} className="fw-bold">
-                  {student.courseFee
-                    ? formatCurrency(student.courseFee)
-                    : formatCurrency(0)}
-                </p>
-              </Col>
-              <Col xs={6} lg={3}>
-                <p style={{ color: "var(--offBlack)" }} className="mb-0">
-                  Payment Status
-                </p>
-                <p
-                  style={{ color: "var(--mainBlue)" }}
-                  className={`text-capitalize fw-bold ${
-                    student.paymentStatus === "part"
-                      ? "text-danger"
-                      : "text-success"
-                  }`}
-                >
-                  {student.paymentStatus}
-                </p>
-              </Col>
-              <Col xs={12} lg={3}>
-                <MyButton
-                  variant="danger"
-                  text="Send Reminder"
-                  className="fw-bold"
-                />
-              </Col>
-            </Row>
-            <hr />
-            <Table hover responsive>
-              <thead>
-                <tr className="small">
-                  <th>Payment Method</th>
-                  <th>Amount Paid</th>
-                  <th>Balance</th>
-                  <th>Comments</th>
-                  <th className="text-primary">
-                    <MdEdit />
-                    Add Payment Record
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="border cursor">
-                <tr>
-                  {student?.payments?.map((info, i) => (
-                    <>
-                      <td style={tstyle} className="text-capitalize">
-                        <div className="mt-2">
-                          <p className="mb-0">Bank Transfer</p>
-                          <Image
-                            key={i}
-                            src={info.receipt}
-                            style={{ width: "20px", height: "20px" }}
-                          />
-                        </div>
-                      </td>
-                      <td style={tstyle} className="text-capitalize">
-                        <div className="mt-2">
-                          <p className="mb-0 text-success">
-                            {formatCurrency(info.amount)}
-                          </p>
-                          <span>
-                            Date:{" "}
-                            {info?.datePaid
-                              ? new Date(info.datePaid).toLocaleDateString(
-                                  "en-GB",
-                                  {
-                                    day: "2-digit",
-                                    month: "2-digit",
-                                    year: "numeric",
-                                  }
-                                )
-                              : null}
-                          </span>
-                        </div>
-                      </td>
-                    </>
-                  ))}
-                  <td style={tstyle} className="text-capitalize">
-                    <div className="mt-2">
-                      <p className="mb-0 text-danger">
-                        {formatCurrency(student.balance)}
-                      </p>
-                    </div>
-                  </td>
-                  <td style={tstyle} className="text-capitalize">
-                    <div className="mt-2">
-                      <p className="mb-0">Payment manipulated by Mubarak</p>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div>
+            <>
+              <Row
+                className={`rounded-3 m-2 p-3 align-items-center ${styles.paymentBg}`}
+              >
+                <Col xs={6} lg={3}>
+                  <div
+                    style={{ color: "var(--deepBlack)" }}
+                    className="fw-bold mb-1"
+                  >
+                    {student.fullName}
+                  </div>
+                  <p style={{ color: "var(--offBlack)" }}>
+                    {student.courseCohort}
+                  </p>
+                </Col>
+                <Col xs={6} lg={3}>
+                  <p style={{ color: "var(--offBlack)" }} className="mb-0">
+                    Course Fee
+                  </p>
+                  <p style={{ color: "var(--mainBlue)" }} className="fw-bold">
+                    {student.courseFee
+                      ? formatCurrency(student.courseFee)
+                      : formatCurrency(0)}
+                  </p>
+                </Col>
+                <Col xs={6} lg={3}>
+                  <p style={{ color: "var(--offBlack)" }} className="mb-0">
+                    Payment Status
+                  </p>
+                  <p
+                    style={{ color: "var(--mainBlue)" }}
+                    className={`text-capitalize fw-bold ${
+                      student.paymentStatus === "part"
+                        ? "text-danger"
+                        : "text-success"
+                    }`}
+                  >
+                    {student.paymentStatus}
+                  </p>
+                </Col>
+                <Col xs={12} lg={3}>
+                  <MyButton
+                    variant="danger"
+                    text="Send Reminder"
+                    className="fw-bold"
+                  />
+                </Col>
+              </Row>
+              <hr />
+              <Table hover responsive>
+                <thead>
+                  <tr className="small">
+                    <th>Payment Method</th>
+                    <th>Amount Paid</th>
+                    <th>Balance</th>
+                    <th>Comments</th>
+                    <th className="text-primary">
+                      <MdEdit />
+                      Add Payment Record
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="border cursor">
+                  <tr>
+                    {student?.payments?.map((info, i) => (
+                      <>
+                        <td style={tstyle} className="text-capitalize">
+                          <div className="mt-2">
+                            <p className="mb-0">Bank Transfer</p>
+                            <Image
+                              key={i}
+                              src={info.receipt}
+                              style={{ width: "20px", height: "20px" }}
+                            />
+                          </div>
+                        </td>
+                        <td style={tstyle} className="text-capitalize">
+                          <div className="mt-2">
+                            <p className="mb-0 text-success">
+                              {formatCurrency(info.amount)}
+                            </p>
+                            <span>
+                              Date:{" "}
+                              {info?.datePaid
+                                ? new Date(info.datePaid).toLocaleDateString(
+                                    "en-GB",
+                                    {
+                                      day: "2-digit",
+                                      month: "2-digit",
+                                      year: "numeric",
+                                    }
+                                  )
+                                : null}
+                            </span>
+                          </div>
+                        </td>
+                      </>
+                    ))}
+                    <td style={tstyle} className="text-capitalize">
+                      <div className="mt-2">
+                        <p className="mb-0 text-danger">
+                          {formatCurrency(student.balance)}
+                        </p>
+                      </div>
+                    </td>
+                    <td style={tstyle} className="text-capitalize">
+                      <div className="mt-2">
+                        <p className="mb-0">Payment manipulated by Mubarak</p>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </>
+          </div>
+        )}
       </MyModal>
     </>
   );
